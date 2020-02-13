8234daa46579429ec894085af53a95e02af7d1dd
Norovbanzad <nbanzad@gmail.com>
2020-02-13 11:37:59 -0500

    Created a database using EF Core

---
+++ Data/CustomerContext.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using customer_consumption_api.Models;
+
+namespace customer_consumption_api.Data
+{
+    public class CustomerContext : DbContext
+    {
+        public CustomerContext(DbContextOptions<CustomerContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Location> Locations { get; set; }
+        public DbSet<CustomerLocation> CustomerLocations { get; set; }
+        public DbSet<Meter> Meters { get; set; }
+        public DbSet<MeterLocation> MeterLocations { get; set; }
+        public DbSet<MeterInterval> MeterIntervals { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            
+        }
+
+    }
+}
+
---
+++ Models/MeterLocation.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace customer_consumption_api.Models
+{
+    public class MeterLocation
+    {
+        [Key]
+        public string MeterId { get; set; }
+        public string LocationId { get; set; }
+        public DateTime ActiveDate { get; set; }
+        public DateTime InactiveDate { get; set; }
+
+        // 'Meters table' and 'Locations table' has many to many relationship.
+        // We have a bridge table 'MeterLocations'.
+        public Location Location { get; set; }
+        public Meter Meter { get; set; }
+    }
+}
