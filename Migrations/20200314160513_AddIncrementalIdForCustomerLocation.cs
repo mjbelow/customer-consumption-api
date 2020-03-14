c1da03fa85a4fe4e77332b914c16a075eef25d80
Matthew Below <55146074+mjbelow@users.noreply.github.com>
2020-03-14 12:11:54 -0400

    chore: create new migration for customerLocation id

---
+++ Migrations/20200314160513_AddIncrementalIdForCustomerLocation.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace customer_consumption_api.Migrations
+{
+    public partial class AddIncrementalIdForCustomerLocation : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Id",
+                table: "CustomerLocations",
+                nullable: false,
+                defaultValue: 0)
+                .Annotation("SqlServer:Identity", "1, 1");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Id",
+                table: "CustomerLocations");
+        }
+    }
+}
